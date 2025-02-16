184526e40e39e0d10ef116a47e8f7f5896e7094d
James McDonald <james@jamesmcdonald.com>
2025-02-16 14:26:21 +0100

    Add generated version

--- cmd/buckettest/buckettest.go
+++ cmd/buckettest/buckettest.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -15,9 +16,17 @@ import (
 // any authentication so you can test that Workload Identity is providing the
 // proper access.
 
+//go:generate go run genversion.go
+
 func main() {
 	bucketName := flag.String("bucket", os.Getenv("BUCKET"), "Bucket to use")
+	versionFlag := flag.Bool("version", false, "Print version and exit")
 	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("buckettest version %s [%s]\n", version, commit)
+		os.Exit(0)
+	}
 
 	if *bucketName == "" {
 		slog.Error("No bucket specified")
